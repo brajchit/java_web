0e022e235622f26c6744c6a3784facef511130f5
odmoreno <oscarpol95@gmail.com>
2016-07-20 11:04:17 -0500

    Modulos
    Clase usuarios

--- src/java/modelos/Usuario.java
+++ src/java/modelos/Usuario.java
@@ -10,5 +10,38 @@ package modelos;
  * @author estudiante.2016
  */
 public class Usuario {
+    private String nombre;
+    private String apellido;
+    private String rol;
+
+    public Usuario(String nombre, String apellido, String rol) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.rol = rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
     
 }
